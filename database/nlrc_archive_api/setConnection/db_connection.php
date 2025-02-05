3281792e73389f854919276212699f24a337b0cf
Faller29 <148046807+Faller29@users.noreply.github.com>
2025-02-05 20:30:32 +0800

    Update db_connection.php

--- database/nlrc_archive_api/setConnection/db_connection.php
+++ database/nlrc_archive_api/setConnection/db_connection.php
@@ -1,7 +1,7 @@
 <?php
 
 function dbconnection() {
-    $con = mysqli_connect("$serverIP", "root", "", "nlrc_archive");
+    $con = mysqli_connect("localhost", "root", "", "nlrc_archive");
 
     if (!$con) {
         die("Database connection failed: " . mysqli_connect_error());
